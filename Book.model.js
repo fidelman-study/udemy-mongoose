dcd5e07482161df81a09a64ddb4d6e5100341cda
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-07-16 14:26:36 +0200

    create schema

--- Book.model.js
+++ Book.model.js
@@ -3,22 +3,8 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
   title: String,
-  publishedDate: {
-    type: Date,
-    default: Date.now
-  },
-  keywords: Array,
-  published: Boolean,
-  author: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  },
-  detail: {
-    modelNumber: Number,
-    hardcover: Boolean,
-    reviews: Number,
-    rank: Number
-  }
+  author: String,
+  category: String
 });
 
 module.exports = mongoose.model('Boock', BookSchema);
