6735ecca1e2aefe6726904119387d479afabea4f
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-07-16 14:35:56 +0200

    connect to DB

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express =require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Book = require('./Book.model');
 
 const app = express();
 const port = 8080;
+const db = 'mongodb://localhost/udemy-mongoose';
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db)
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Database connected');
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.log(`Database connection failed: ${error}`);
+  });
 
 app.listen(port, () => {
   console.log('app listening on port ' + port);
